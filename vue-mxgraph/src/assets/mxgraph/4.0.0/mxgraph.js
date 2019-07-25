fa6d3c7635df69ad734becfb4493fa42b1b8628d
eko.zhan <eko.z@outlook.com>
2019-07-25 13:27:09 +0800

    fix: mxgraph load from xml

--- vue-mxgraph/src/assets/mxgraph/4.0.0/mxgraph.js
+++ vue-mxgraph/src/assets/mxgraph/4.0.0/mxgraph.js
@@ -1,12 +1,25 @@
 let mxgraphFactory = require('mxgraph')
+// comment by eko.zhan at 2019-07-24 16:00
+// fixme mxBasePath 等参数无效，目前是在 public/index.html 中定义
 const factory = new mxgraphFactory({
-  mxImageBasePath: 'assets/mxgraph/4.0.0/images',
-  mxBasePath: 'assets/mxgraph/4.0.0'
+  mxImageBasePath: './static/images',
+  mxBasePath: './static'
 })
 
 // let _default = {}
 // for (var name in factory) {
 //   _default[name] = factory[name]
 // }
+
+// Editor doesn't created when mxGraph is used as npm module
+// see https://github.com/jgraph/mxgraph/issues/49
+window.mxGraph = factory.mxGraph
+window.mxGraphModel = factory.mxGraphModel
+window.mxEditor = factory.mxEditor
+window.mxGeometry = factory.mxGeometry
+window.mxDefaultKeyHandler = factory.mxDefaultKeyHandler
+window.mxDefaultPopupMenu = factory.mxDefaultPopupMenu
+window.mxStylesheet = factory.mxStylesheet
+window.mxDefaultToolbar = factory.mxDefaultToolbar
 
 module.exports = factory
